d62e9001e8b15314ab46ea8c450ba5b7487f904c
takako-K <takako.k828@gmail.com>
2020-12-13 06:18:38 +0000

    [Review]Rails⑦

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3af5a05b4f9ca323cd112c8572b0f620a841d293cb8929769bc8b383b5c16868b3fc3a8321504fd9786dce53424db60796e9558c547122a6921dbcf5deb84a91'
